cc41633e84739225d21a507f75c4b90536756edf
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2026-01-15 08:53:23 +0100

    fix: merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from "./Components/Homepage_components/Hero";
-
+import Limitations from "./Components/Homepage_components/Limitations";
 
 export default function Home() {
   return (
-   <>
-   <Hero/>
-   </>
+    <>
+      <Hero />
+      <Limitations />
+    </>
   );
 }
