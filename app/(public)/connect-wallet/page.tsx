fcf5e5689be5f78252eb0e62bf0f1f107a97b1eb
chiscookeke11 <chiscookeke11@gmail.com>
2026-01-23 11:37:33 +0100

    fixed merge conflict

--- app/(public)/connect-wallet/page.tsx
+++ app/(public)/connect-wallet/page.tsx
@@ -1,10 +1,12 @@
+import type { Metadata } from "next";
 import ConnectWalletPage from "@/app/Components/connect-wallet-page/ConnectWalletPage";
 
+export const metadata: Metadata = {
+  title: "Connect Wallet | Offline Bridge",
+  description: "Securely connect your wallet to access Offline Bridge services.",
+  keywords: ["Connect Wallet", "Web3", "Crypto Wallet", "Offline Bridge"],
+};
 
 export default function Page() {
-  return (
-    <>
-      <ConnectWalletPage />
-    </>
-  );
+  return <ConnectWalletPage />;
 }
